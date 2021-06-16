921f47009b96ea6b8fe99231efa46f9d12b491b3
Arpit Byahut <85457750+arpitbyahut@users.noreply.github.com>
2021-06-17 02:57:19 +0530

    Update and rename server.original.js to server.js

--- src/config/server.js
+++ src/config/server.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TELEGRAM_TOKEN: "",
+  TELEGRAM_TOKEN: "1826450859:AAFslnR6SqLBUBW3dAJJhfyOu7bhZKGrJ_I",
   MIN_SECONDS_BETWEEN_SENDS: 30, // Minimum seconds between send in the same group
   SECONDS_CYCLE: 5, // Seconds in each test cycle
 }
